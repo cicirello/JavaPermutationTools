a713a301dcd9c5971fe9a0bf87c1fb0d529ab258
Vincent Cicirello <cicirello@users.noreply.github.com>
2018-08-02 16:00:38 -0400

    updated copyright header, disclaimer, etc

--- src/org/cicirello/permutations/Permutation.java
+++ src/org/cicirello/permutations/Permutation.java
@@ -1,23 +1,22 @@
 /*
  * Copyright 2005, 2010, 2014-2018 Vincent A. Cicirello, <https://www.cicirello.org/>.
  *
- * This file is part of package org.cicirello.permutations.
+ * This file is part of JavaPermutationTools (https://jpt.cicirello.org/).
  *
- * Java package org.cicirello.permutations is free software: you can 
+ * JavaPermutationTools is free software: you can 
  * redistribute it and/or modify it under the terms of the GNU 
  * General Public License as published by the Free Software 
  * Foundation, either version 3 of the License, or (at your 
  * option) any later version.
  *
- * Java package org.cicirello.permutations is distributed in the hope 
+ * JavaPermutationTools is distributed in the hope 
  * that it will be useful, but WITHOUT ANY WARRANTY; without even 
  * the implied warranty of MERCHANTABILITY or FITNESS FOR A 
  * PARTICULAR PURPOSE.  See the GNU General Public License for more 
  * details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Java package org.cicirello.permutations.  If not, 
- * see <http://www.gnu.org/licenses/>.
+ * along with JavaPermutationTools.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 package org.cicirello.permutations;
