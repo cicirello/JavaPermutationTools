500d75acb4aafc3afa86aa82f1b0647239de28aa
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2021-01-29 17:10:42 -0500

    improved test coverage

--- tests/org/cicirello/sequences/distance/SequenceDistanceTests.java
+++ tests/org/cicirello/sequences/distance/SequenceDistanceTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 Vincent A. Cicirello, <https://www.cicirello.org/>.
+ * Copyright 2018-2021 Vincent A. Cicirello, <https://www.cicirello.org/>.
  *
  * This file is part of JavaPermutationTools (https://jpt.cicirello.org/).
  *
@@ -724,7 +724,9 @@ public class SequenceDistanceTests {
 	@Test
 	public void testTauObjectSequences() {
 		KendallTauSequenceDistance d = new KendallTauSequenceDistance();
-		for (int n = 2; n <= 10; n++) {
+		assertEquals(0, d.distance(new String[0], new String[0]));
+		assertEquals(0, d.distance(new ArrayList<String>(), new ArrayList<String>())); 
+		for (int n = 2; n <= 8; n++) {
 			//maximal distance if all unique elements (i.e., a permutation) is reversed sequence
 			String[] s1 = new String[n];
 			String[] s2 = new String[n];
@@ -747,7 +749,7 @@ public class SequenceDistanceTests {
 			assertEquals("end points swapped", expected, d.distance(toList(s1),toList(s3)));
 			assertEquals("end points swapped", expected, d.distance(toList(s3),toList(s1)));
 		}
-		for (int n = 2; n <= 10; n++) {
+		for (int n = 2; n <= 8; n++) {
 			//maximal distance if all unique elements (i.e., a permutation) is reversed sequence
 			NonComparable[] s1 = new NonComparable[n];
 			NonComparable[] s2 = new NonComparable[n];
@@ -773,7 +775,7 @@ public class SequenceDistanceTests {
 	@Test
 	public void testTauAlg2ObjectSequences() {
 		KendallTauSequenceDistance d = new KendallTauSequenceDistance(true);
-		for (int n = 2; n <= 10; n++) {
+		for (int n = 2; n <= 8; n++) {
 			//maximal distance if all unique elements (i.e., a permutation) is reversed sequence
 			String[] s1 = new String[n];
 			String[] s2 = new String[n];
@@ -796,7 +798,7 @@ public class SequenceDistanceTests {
 			assertEquals("end points swapped", expected, d.distance(toList(s1),toList(s3)));
 			assertEquals("end points swapped", expected, d.distance(toList(s3),toList(s1)));
 		}
-		for (int n = 2; n <= 10; n++) {
+		for (int n = 2; n <= 8; n++) {
 			//maximal distance if all unique elements (i.e., a permutation) is reversed sequence
 			NonComparable[] s1 = new NonComparable[n];
 			NonComparable[] s2 = new NonComparable[n];
@@ -820,11 +822,168 @@ public class SequenceDistanceTests {
 	}
 	
 	@Test
+	public void testKendallTauDistanceExceptions() {
+		final KendallTauSequenceDistance d = new KendallTauSequenceDistance();
+		
+		IllegalArgumentException thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(new int[3], new int[4])
+		);
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(new long[3], new long[4])
+		);
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(new short[3], new short[4])
+		);
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(new byte[3], new byte[4])
+		);
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(new double[3], new double[4])
+		);
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(new float[3], new float[4])
+		);
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(new boolean[3], new boolean[4])
+		);
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(new char[3], new char[4])
+		);
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(new String[3], new String[4])
+		);
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance("hello", "hello again")
+		);
+		final ArrayList<String> s1 = new ArrayList<String>();
+		final ArrayList<String> s2 = new ArrayList<String>();
+		s1.add("a");
+		s1.add("b");
+		s2.add("a");
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(s1, s2)
+		);
+	}
+	
+	@Test
+	public void testKendallTauDistanceExceptionsDiffElements() {
+		final KendallTauSequenceDistance d = new KendallTauSequenceDistance();
+		helperKendallTauDistanceExceptionsDiffElements(d);
+	}
+	
+	@Test
+	public void testKendallTauDistanceExceptionsDiffElementsAlg2() {
+		final KendallTauSequenceDistance d = new KendallTauSequenceDistance(true);
+		helperKendallTauDistanceExceptionsDiffElements(d);
+	}
+	
+	private void helperKendallTauDistanceExceptionsDiffElements(final KendallTauSequenceDistance d) {
+	
+		final int[] i1 = { 1, 2, 3 };
+		final int[] i2 = { 1, 2, 4 };
+		IllegalArgumentException thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(i1, i2)
+		);
+		final String[] str1 = {"a", "a", "c"};
+		final String[] str2 = {"a", "a", "d"};
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(str1, str2)
+		);
+		final ArrayList<String> aL1 = new ArrayList<String>();
+		final ArrayList<String> aL2 = new ArrayList<String>();
+		for (String e : str1) aL1.add(e);
+		for (String e : str2) aL2.add(e);
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(aL1, aL2)
+		);
+		final double[] d1 = { 1, 2, 3 };
+		final double[] d2 = { 1, 2, 4 };
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(d1, d2)
+		);
+		final float[] f1 = { 1f, 2f, 3f };
+		final float[] f2 = { 1f, 2f, 4f };
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(f1, f2)
+		);
+		final long[] L1 = { 1, 2, 3 };
+		final long[] L2 = { 1, 2, 4 };
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(L1, L2)
+		);
+		final short[] sh1 = { 1, 2, 3 };
+		final short[] sh2 = { 1, 2, 4 };
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(sh1, sh2)
+		);
+		final byte[] b1 = { 1, 2, 3 };
+		final byte[] b2 = { 1, 2, 4 };
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(b1, b2)
+		);
+		final char[] ch1 = { '1', '2', '3' };
+		final char[] ch2 = { '1', '2', '4' };
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(ch1, ch2)
+		);
+		final String g1 = "123";
+		final String g2 = "124";
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(g1, g2)
+		);
+		final boolean[] bool1 = { false, false, true };
+		final boolean[] bool2 = { false, false, false };
+		thrown = assertThrows( 
+			IllegalArgumentException.class,
+			() -> d.distance(bool1, bool2)
+		);
+	}
+	
+	@Test
 	public void testKendallTauDistance() {
 		KendallTauSequenceDistance d = new KendallTauSequenceDistance();
 		identicalSequences(d);
+		helperForKendallTauCases(d);
+	}
+	
+	@Test
+	public void testKendallTauDistanceAlg2() {
+		KendallTauSequenceDistance d = new KendallTauSequenceDistance(true);
+		identicalSequences(d);
+		helperForKendallTauCases(d);
+	}
+	
+	@Test
+	public void testKendallTauDistanceAlg1() {
+		KendallTauSequenceDistance d = new KendallTauSequenceDistance(false);
+		identicalSequences(d);
+		helperForKendallTauCases(d);
+	}
+	
+	private void helperForKendallTauCases(KendallTauSequenceDistance d) {
 		// test first with simpler cases: all unique elements
-		for (int n = 2; n <= 10; n++) {
+		for (int n = 2; n <= 8; n++) {
 			//maximal distance if all unique elements (i.e., a permutation) is reversed sequence
 			int[] s1 = new int[n];
 			int[] s2 = new int[n];
@@ -1171,276 +1330,7 @@ public class SequenceDistanceTests {
 		}
 	}
 	
-	@Test
-	public void testKendallTauDistanceAlg2() {
-		KendallTauSequenceDistance d = new KendallTauSequenceDistance(true);
-		identicalSequences(d);
-		// test first with simpler cases: all unique elements
-		for (int n = 2; n <= 10; n++) {
-			//maximal distance if all unique elements (i.e., a permutation) is reversed sequence
-			int[] s1 = new int[n];
-			int[] s2 = new int[n];
-			int[] s3 = new int[n];
-			for (int i = 0; i < n; i++) {
-				s3[i] = s1[i] = s2[n-1-i] = i+2;
-				// deliberately didn't use 0 to n-1 for a bit of white box testing (i.e., testing the indexing into arrays of queues)
-			}
-			s3[0] = s2[0];
-			s3[n-1] = s2[n-1];
-			int expected = n*(n-1)/2;
-			assertEquals("maximal distance", expected, d.distance(s1,s2));
-			assertEquals("maximal distance", expected, d.distance(s2,s1));
-			expected = 2*n-3;
-			assertEquals("end points swapped", expected, d.distance(s1,s3));
-			assertEquals("end points swapped", expected, d.distance(s3,s1));
-			{ // long
-				long[] t1 = new long[n];
-				long[] t2 = new long[n];
-				long[] t3 = new long[n];
-				for (int i = 0; i < n; i++) {
-					t1[i] = s1[i]; t2[i] = s2[i]; t3[i] = s3[i];
-				}
-				expected = n*(n-1)/2;
-				assertEquals("maximal distance", expected, d.distance(t1,t2));
-				assertEquals("maximal distance", expected, d.distance(t2,t1));
-				expected = 2*n-3;
-				assertEquals("end points swapped", expected, d.distance(t1,t3));
-				assertEquals("end points swapped", expected, d.distance(t3,t1));
-			}
-			{ // short
-				short[] t1 = new short[n];
-				short[] t2 = new short[n];
-				short[] t3 = new short[n];
-				for (int i = 0; i < n; i++) {
-					t1[i] = (short)s1[i]; t2[i] = (short)s2[i]; t3[i] = (short)s3[i];
-				}
-				expected = n*(n-1)/2;
-				assertEquals("maximal distance", expected, d.distance(t1,t2));
-				assertEquals("maximal distance", expected, d.distance(t2,t1));
-				expected = 2*n-3;
-				assertEquals("end points swapped", expected, d.distance(t1,t3));
-				assertEquals("end points swapped", expected, d.distance(t3,t1));
-			}
-			{ // byte
-				byte[] t1 = new byte[n];
-				byte[] t2 = new byte[n];
-				byte[] t3 = new byte[n];
-				for (int i = 0; i < n; i++) {
-					t1[i] = (byte)s1[i]; t2[i] = (byte)s2[i]; t3[i] = (byte)s3[i];
-				}
-				expected = n*(n-1)/2;
-				assertEquals("maximal distance", expected, d.distance(t1,t2));
-				assertEquals("maximal distance", expected, d.distance(t2,t1));
-				expected = 2*n-3;
-				assertEquals("end points swapped", expected, d.distance(t1,t3));
-				assertEquals("end points swapped", expected, d.distance(t3,t1));
-			}
-			{ // char
-				char[] t1 = new char[n];
-				char[] t2 = new char[n];
-				char[] t3 = new char[n];
-				for (int i = 0; i < n; i++) {
-					t1[i] = (char)s1[i]; t2[i] = (char)s2[i]; t3[i] = (char)s3[i];
-				}
-				expected = n*(n-1)/2;
-				assertEquals("maximal distance", expected, d.distance(t1,t2));
-				assertEquals("maximal distance", expected, d.distance(t2,t1));
-				expected = 2*n-3;
-				assertEquals("end points swapped", expected, d.distance(t1,t3));
-				assertEquals("end points swapped", expected, d.distance(t3,t1));
-				String u1 = new String(t1);
-				String u2 = new String(t2);
-				String u3 = new String(t3);
-				expected = n*(n-1)/2;
-				assertEquals("maximal distance", expected, d.distance(u1,u2));
-				assertEquals("maximal distance", expected, d.distance(u2,u1));
-				expected = 2*n-3;
-				assertEquals("end points swapped", expected, d.distance(u1,u3));
-				assertEquals("end points swapped", expected, d.distance(u3,u1));
-			}
-			{ // float
-				float[] t1 = new float[n];
-				float[] t2 = new float[n];
-				float[] t3 = new float[n];
-				for (int i = 0; i < n; i++) {
-					t1[i] = s1[i]; t2[i] = s2[i]; t3[i] = s3[i];
-				}
-				expected = n*(n-1)/2;
-				assertEquals("maximal distance", expected, d.distance(t1,t2));
-				assertEquals("maximal distance", expected, d.distance(t2,t1));
-				expected = 2*n-3;
-				assertEquals("end points swapped", expected, d.distance(t1,t3));
-				assertEquals("end points swapped", expected, d.distance(t3,t1));
-			}
-			{ // double
-				double[] t1 = new double[n];
-				double[] t2 = new double[n];
-				double[] t3 = new double[n];
-				for (int i = 0; i < n; i++) {
-					t1[i] = s1[i]; t2[i] = s2[i]; t3[i] = s3[i];
-				}
-				expected = n*(n-1)/2;
-				assertEquals("maximal distance", expected, d.distance(t1,t2));
-				assertEquals("maximal distance", expected, d.distance(t2,t1));
-				expected = 2*n-3;
-				assertEquals("end points swapped", expected, d.distance(t1,t3));
-				assertEquals("end points swapped", expected, d.distance(t3,t1));
-			}
-		}
-		Permutation p = new Permutation(6);
-		int[] s1 = new int[6];
-		for (int i = 0; i < 6; i++) s1[i] = p.get(i);
-		int[] s2 = new int[6];
-		for (Permutation q : p) {
-			for (int i = 0; i < 6; i++) s2[i] = q.get(i);
-			int expected = naiveKendalTau(s1,s2);
-			assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(s1,s2));
-			assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(s2,s1));
-		}
-		{ // long
-			s1 = new int[5];
-			s2 = new int[5];
-			Permutation r = new Permutation(5);
-			long[] t1 = new long[5];
-			for (int i = 0; i < 5; i++) t1[i] = s1[i] = r.get(i);
-			long[] t2 = new long[5];
-			for (Permutation q : r) {
-				for (int i = 0; i < 5; i++) t2[i] = s2[i] = q.get(i);
-				int expected = naiveKendalTau(s1,s2);
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(t1,t2));
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(t2,t1));
-			}
-		}
-		{ // short
-			s1 = new int[5];
-			s2 = new int[5];
-			Permutation r = new Permutation(5);
-			short[] t1 = new short[5];
-			for (int i = 0; i < 5; i++) s1[i] = t1[i] = (short)r.get(i);
-			short[] t2 = new short[5];
-			for (Permutation q : r) {
-				for (int i = 0; i < 5; i++) s2[i] = t2[i] = (short)q.get(i);
-				int expected = naiveKendalTau(s1,s2);
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(t1,t2));
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(t2,t1));
-			}
-		}
-		{ // byte
-			s1 = new int[5];
-			s2 = new int[5];
-			Permutation r = new Permutation(5);
-			byte[] t1 = new byte[5];
-			for (int i = 0; i < 5; i++) s1[i] = t1[i] = (byte)r.get(i);
-			byte[] t2 = new byte[5];
-			for (Permutation q : r) {
-				for (int i = 0; i < 5; i++) s2[i] = t2[i] = (byte)q.get(i);
-				int expected = naiveKendalTau(s1,s2);
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(t1,t2));
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(t2,t1));
-			}
-		}
-		{ // char
-			s1 = new int[5];
-			s2 = new int[5];
-			Permutation r = new Permutation(5);
-			char[] t1 = new char[5];
-			for (int i = 0; i < 5; i++) s1[i] = t1[i] = (char)r.get(i);
-			String u1 = new String(t1);
-			char[] t2 = new char[5];
-			for (Permutation q : r) {
-				for (int i = 0; i < 5; i++) s2[i] = t2[i] = (char)q.get(i);
-				String u2 = new String(t2);
-				int expected = naiveKendalTau(s1,s2);
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(t1,t2));
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(t2,t1));
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(u1,u2));
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(u2,u1));
-			}
-		}
-		{ // float
-			s1 = new int[5];
-			s2 = new int[5];
-			Permutation r = new Permutation(5);
-			float[] t1 = new float[5];
-			for (int i = 0; i < 5; i++) t1[i] = s1[i] = r.get(i);
-			float[] t2 = new float[5];
-			for (Permutation q : r) {
-				for (int i = 0; i < 5; i++) t2[i] = s2[i] = q.get(i);
-				int expected = naiveKendalTau(s1,s2);
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(t1,t2));
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(t2,t1));
-			}
-		}
-		{ // double
-			s1 = new int[5];
-			s2 = new int[5];
-			Permutation r = new Permutation(5);
-			double[] t1 = new double[5];
-			for (int i = 0; i < 5; i++) t1[i] = s1[i] = r.get(i);
-			double[] t2 = new double[5];
-			for (Permutation q : r) {
-				for (int i = 0; i < 5; i++) t2[i] = s2[i] = q.get(i);
-				int expected = naiveKendalTau(s1,s2);
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(t1,t2));
-				assertEquals("checking consistence with naive implementation of unique element version", expected, d.distance(t2,t1));
-			}
-		}
-		// Now test with duplicate elements
-		String t1 = "abcdaabb";
-		String t2 = "dcbababa";
-		assertEquals("case where discordant pair counting fails", 9, d.distance(t1,t2));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(t2,t1));
-		char[] c1 = t1.toCharArray();
-		char[] c2 = t2.toCharArray();
-		long[] L1 = new long[c1.length];
-		long[] L2 = new long[c2.length];
-		short[] sh1 = new short[c1.length];
-		short[] sh2 = new short[c2.length];
-		byte[] b1 = new byte[c1.length];
-		byte[] b2 = new byte[c2.length];
-		int[] i1 = new int[c1.length];
-		int[] i2 = new int[c2.length];
-		float[] f1 = new float[c1.length];
-		float[] f2 = new float[c2.length];
-		double[] d1 = new double[c1.length];
-		double[] d2 = new double[c2.length];
-		for (int i = 0; i < c1.length; i++) {
-			L1[i] = i1[i] = c1[i];
-			d1[i] = f1[i] = i1[i];
-			sh1[i] = (short)c1[i];
-			b1[i] = (byte)c1[i];
-			L2[i] = i2[i] = c2[i];
-			d2[i] = f2[i] = i2[i];
-			sh2[i] = (short)c2[i];
-			b2[i] = (byte)c2[i];
-		}
-		assertEquals("case where discordant pair counting fails", 9, d.distance(c1,c2));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(c2,c1));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(L1,L2));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(L2,L1));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(sh1,sh2));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(sh2,sh1));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(b1,b2));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(b2,b1));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(i1,i2));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(i2,i1));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(f1,f2));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(f2,f1));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(d1,d2));
-		assertEquals("case where discordant pair counting fails", 9, d.distance(d2,d1));
-		for (int n = 2; n < 8; n++) {
-			for (int i = 1; i < n; i++) {
-				boolean[] a1 = new boolean[n];
-				boolean[] a2 = new boolean[n];
-				for (int j = 0; j < i; j++) {
-					a2[j] = a1[n-1-j] = true;
-				}
-				int expected = i * (n-i);
-				assertEquals("boolean case", expected, d.distance(a2,a1));
-				assertEquals("boolean case", expected, d.distance(a1,a2));
-			}
-		}
-	}
+	
 	
 	// simple naive O(n^2) version if elements are all unique
 	private int naiveKendalTau(int[] s1, int[] s2) {
